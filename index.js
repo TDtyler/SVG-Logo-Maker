f65b4bdebbcf53983f3e97d198544189215d0316
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-06-03 09:05:11 -0700

    index.js write file to svg,choose shape, init app

--- index.js
+++ index.js
@@ -12,9 +12,59 @@ const questions = [
     {
         type: 'input',
         name: 'textColor',
-        message: 'Enter text color (keyword or hex)'
+        message: 'Enter text color (keyword or hex)',
     },
     {
-        type: 'in'
+        type: 'list',
+        name: 'shape',
+        message: 'choose a shape',
+        choices: ['Square', 'Circle', 'Triangle' ],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hex)',
+    },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    const { text, textColor, shape, shapeColor } = answers;
+    const shapeObj = new Shape();
+    let svgElement = '';
+
+    shapeObj.setColor(shapeColor);
+
+    switch (shape) {
+        case 'Square':
+            const square = new Square();
+            square.setColor(shapeColor);
+            svgElement = square.render();
+            break;
+        case 'Circle':
+            const circle = new Circle();
+            circle.setColor(shapeColor);
+            svgElement = circle.render();
+            break;
+        case 'Triangle':
+            const triangle = new Triangle();
+            triangle.setColor(shapeColor);
+            svgElement = triangle.render();
+            break;
     }
-]
+
+    let x = 150, y = 120;
+    if (shape === 'Triangle') {
+        y = 135;
+    } else if (shape === 'Square') {
+        y = 145;
+    }
+
+    const finalSvg = `<svg xmlns "http://www.w3.org/2000/svg" width="300" height="200">
+        ${svgElement}
+        <text x="${x}" y="${y}" fill="${textColor}" font-size="50" text-anchor="middle">${text}</text>
+        </svg>`;
+
+        fs.writeFileSync('logo.svg', finalSvg);
+
+        console.log('Generated logo.svg');
+});
